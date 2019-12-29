6bdb151e245eab6ab753a9d7366ec258cb8ac022
JasonHeyward <mddarr@gmail.com>
2019-12-29 15:59:02 -0800

    First commit

--- backend/fileuploadexample/settings.py
+++ backend/fileuploadexample/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'fileuploadexample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'database1',
+        'USER': 'database1_role',
+        'PASSWORD': 'database1_password',
+        'HOST': 'db',
+        'PORT': '5432',
+    },
 }
 
 
