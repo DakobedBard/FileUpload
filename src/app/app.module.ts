79241b718dca4da7439d86e5d71c83224b53cb90
JasonHeyward <mddarr@gmail.com>
2019-12-29 19:30:28 -0800

    begin to add styles!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DocumentComponent } from './document/document.component';
-
-
+import { MaterialModule } from './material/material.module';
+import { HttpClientModule,HTTP_INTERCEPTORS }    from '@angular/common/http';
+import { TokenInterceptorService } from './token-interceptor.service'
 @NgModule({
   declarations: [
     AppComponent,
-    DocumentComponent
+    DocumentComponent,
+    
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptorService,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
